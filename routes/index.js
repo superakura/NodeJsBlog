8c59652bd67c291f93ecda477e364bd8338ee3d4
gy <gy@example.com>
2017-06-15 14:10:47 +0800

    完成编辑、删除功能

--- routes/index.js
+++ routes/index.js
@@ -203,4 +203,47 @@ router.get('/u/:name/:day/:title', function(req, res) {
     });
 });
 
+router.get('/edit/:name/:day/:title', checkLogin);
+router.get('/edit/:name/:day/:title', function(req, res) {
+    Post.edit(req.params.name, req.params.day, req.params.title, function(err, post) {
+        if (err) {
+            req.flash('error', err);
+            return res.redirect('/');
+        }
+        res.render('edit', {
+            title: '编辑',
+            post: post,
+            user: req.session.user,
+            success: req.flash('success').toString(),
+            error: req.flash('error').toString()
+        });
+    });
+});
+
+router.post('/edit/:name/:day/:title', checkLogin);
+router.post('/edit/:name/:day/:title', function(req, res) {
+    var currentUser = req.session.user;
+    Post.update(currentUser.name, req.params.day, req.params.title, req.body.post, function(err) {
+        var url = encodeURI('/u/' + req.params.name + '/' + req.params.day + '/' + req.params.title);
+        if (err) {
+            req.flash('error', err);
+            return res.redirect(url);
+        }
+        req.flash('success', '修改成功！');
+        res.redirect(url);
+    });
+});
+
+router.get('/remove/:name/:day/:title', checkLogin);
+router.get('/remove/:name/:day/:title', function(req, res) {
+    Post.remove(req.params.name, req.params.day, req.params.title, function(err) {
+        if (err) {
+            req.flash('error', err);
+            return res.redirect('back');
+        }
+        req.flash('success', '删除成功！');
+        res.redirect('/');
+    });
+});
+
 module.exports = router;
