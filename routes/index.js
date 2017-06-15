76473ab7fde836b2e2e15d181d98d5e051fc94cc
gy <gy@example.com>
2017-06-15 09:12:26 +0800

    完成用户页面，文章页面

--- routes/index.js
+++ routes/index.js
@@ -22,7 +22,7 @@ function checkNotLogin(req, res, next) {
 }
 
 router.get('/', function(req, res) {
-    Post.get(null, function(err, posts) {
+    Post.getAll(null, function(err, posts) {
         if (err) {
             posts = [];
         }
@@ -163,6 +163,44 @@ router.post('/upload', checkLogin);
 router.post('/upload', function(req, res) {
     req.flash('success', '文件上传成功！');
     res.redirect('/upload');
+});
+
+router.get('/u/:name', function(req, res) {
+    User.get(req.params.name, function(err, user) {
+        if (!user) {
+            req.flash('error', '用户不存在！');
+            return res.redirect('/');
+        }
+        Post.getAll(user.name, function(err, posts) {
+            if (err) {
+                req.flash('error', err);
+                return res.redirect('/');
+            }
+            res.render('user', {
+                title: user.name,
+                posts: posts,
+                user: req.session.user,
+                success: req.flash('success').toString(),
+                error: req.flash('error').toString()
+            });
+        });
+    });
+});
+
+router.get('/u/:name/:day/:title', function(req, res) {
+    Post.getOne(req.params.name, req.params.day, req.params.title, function(err, post) {
+        if (err) {
+            req.flash('error', err);
+            return res.redirect('/');
+        }
+        res.render('article', {
+            title: req.params.title,
+            post: post,
+            user: req.session.user,
+            success: req.flash('success').toString(),
+            error: req.flash('error').toString()
+        });
+    });
 });
 
 module.exports = router;
